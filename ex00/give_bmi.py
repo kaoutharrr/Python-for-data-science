3cb31d572fff1a9be5e6d0e1ed0c04ab4eca4ca0
Kaouthar Kouaz <kkouaz@e2r3p2.1337.ma>
2025-11-26 18:43:37 +0100

    ex00 done

--- ex00/give_bmi.py
+++ ex00/give_bmi.py
@@ -1,5 +1,25 @@
-import numpy
+import numpy as np
 
 
-def give_bmi(height: list[int | float], weight: list[int | float]) -> list[int | float]:
+def give_bmi(height: list[int | float],
+             weight: list[int | float]) -> list[int | float]:
     """calculates the bmi value of two lists of integers"""
+    try:
+        assert len(height) == len(weight), "Lists should be the same size!"
+        np_height = np.array(height)
+        np_weight = np.array(weight)
+        np_res = np_weight / (np_height ** 2)
+        return (np_res.tolist())
+    except Exception as e:
+        print(f"Error :{e}")
+
+
+def apply_limit(bmi: list[int | float], limit: int) -> list[bool]:
+    """checks if the elements of a list of bmi values are above the limit"""
+    try:
+        my_array = np.array(bmi)
+        res = my_array > limit
+        return res.tolist()
+
+    except Exception as e:
+        print(f"Error :{e}")

c8c1c057ea5922d374735c2bcb907c07547e185f
Kaouthar Kouaz <kkouaz@e2r4p2.1337.ma>
2025-11-28 06:14:20 +0100

    ain't no sunshine when sh's gooooone

--- ex00/load_csv.py
+++ ex00/load_csv.py
@@ -1,1 +1,12 @@
-# ?def load(path: str) -> Dataset:
+import pandas as pd
+
+
+def load(path: str) -> pd.DataFrame:
+    """Writes dimensions of the given data set and returns it"""
+    try:
+        df = pd.read_csv(path)
+        print(f"Loading dataset of dimensions {df.shape}")
+        return df
+    except Exception as e:
+        print(f"Error: {e}")
+        return None

c3b910da9ae8501787d58c42321c4e5ef2acd6ae
Kaouthar Kouaz <kkouaz@e2r4p2.1337.ma>
2025-11-29 02:48:58 +0100

    norminette

--- ex02/aff_pop.py
+++ ex02/aff_pop.py
@@ -8,23 +8,22 @@ def converter(val) -> float:
     if s.endswith('K'):
         return float(s[:-1]) * 1000
     if s.endswith('M'):
-        return float(s[:-1] ) * 1_000_000
+        return float(s[:-1]) * 1_000_000
     return float(s)
+
 
 def main():
     """displays the country information of two countries as a graph"""
-    
+
     try:
         df = load("population_total.csv")
-        # my_country = 'Morocco'
-        # other_campus = 'Japan'
-        target_years = [str(year) for year in range(1800, 2051) ]
+        target_years = [str(year) for year in range(1800, 2051)]
         my_campus = df[df['country'] == 'Morocco'][target_years]
         other_campus = df[df['country'] == 'Japan'][target_years]
         pop_campus = [converter(val) for val in my_campus.values[0]]
         pop_other = [converter(val) for val in other_campus.values[0]]
         years_int = [int(year) for year in target_years]
-        
+
         plt.plot(years_int, pop_campus, label="Morocco")
         plt.plot(years_int, pop_other, label="Japan")
 
@@ -33,8 +32,9 @@ def main():
         plt.ylabel("Population")
         plt.xticks(range(1800, 2051, 40))
         ax = plt.gca()  # Get current axes
-        ax.yaxis.set_major_formatter(plt.FuncFormatter(lambda x, p: f'{x/1e6:.0f}M'))
-        
+        ax.yaxis.set_major_formatter
+        (plt.FuncFormatter(lambda x, p: f'{x/1e6:.0f}M'))
+
         plt.legend()
         plt.show()
     except Exception as e:

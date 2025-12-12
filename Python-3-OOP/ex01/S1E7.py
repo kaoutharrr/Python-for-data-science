cd5f1f9ec96fc1be923b379b439b86305ea4aa0c
Kaouthar Kouaz <kkouaz@e2r3p2.1337.ma>
2025-12-12 04:08:57 +0100

    norminette

--- Python-3-OOP/ex01/S1E7.py
+++ Python-3-OOP/ex01/S1E7.py
@@ -3,18 +3,17 @@ from S1E9 import Character
 
 class Baratheon(Character):
     """the concrete class for Baratheon."""
-    def __init__(self, first_name, is_alive = True):
+    def __init__(self, first_name, is_alive=True):
         """constructor for baratheon class"""
         super().__init__(first_name, is_alive)
         self.family_name = "Baratheon"
         self.eyes = "brown"
         self.hairs = "dark"
-        
-        
+
     def __str__(self):
         """Return string representation of the Baratheon character."""
         return f"Vector('{self.family_name}', '{self.eyes}', '{self.hairs}')"
-    
+
     def __repr__(self):
         """Return official string representation of the Baratheon character"""
         return self.__str__()
@@ -22,7 +21,7 @@ class Baratheon(Character):
 
 class Lannister(Character):
     """Representing the Lannister family."""
-    def __init__(self, first_name, is_alive = True):
+    def __init__(self, first_name, is_alive=True):
         """constructor for Lannister class"""
         super().__init__(first_name, is_alive)
         self.family_name = "Lannister"
@@ -32,7 +31,7 @@ class Lannister(Character):
     def __str__(self):
         """Return string representation of the Lannister character."""
         return f"Vector('{self.family_name}', '{self.eyes}', '{self.hairs}')"
-    
+
     def __repr__(self):
         """Return official string representation of the Lannister character"""
         return self.__str__()

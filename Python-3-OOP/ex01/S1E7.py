c2e43e5567bf89ce35d9a524a9c592eb50104e64
Kaouthar Kouaz <kkouaz@e2r3p2.1337.ma>
2025-12-12 04:03:52 +0100

    if this world was miiiine

---
+++ Python-3-OOP/ex01/S1E7.py
@@ -0,0 +1,43 @@
+from S1E9 import Character
+
+
+class Baratheon(Character):
+    """the concrete class for Baratheon."""
+    def __init__(self, first_name, is_alive = True):
+        """constructor for baratheon class"""
+        super().__init__(first_name, is_alive)
+        self.family_name = "Baratheon"
+        self.eyes = "brown"
+        self.hairs = "dark"
+        
+        
+    def __str__(self):
+        """Return string representation of the Baratheon character."""
+        return f"Vector('{self.family_name}', '{self.eyes}', '{self.hairs}')"
+    
+    def __repr__(self):
+        """Return official string representation of the Baratheon character"""
+        return self.__str__()
+
+
+class Lannister(Character):
+    """Representing the Lannister family."""
+    def __init__(self, first_name, is_alive = True):
+        """constructor for Lannister class"""
+        super().__init__(first_name, is_alive)
+        self.family_name = "Lannister"
+        self.eyes = "blue"
+        self.hairs = "light"
+
+    def __str__(self):
+        """Return string representation of the Lannister character."""
+        return f"Vector('{self.family_name}', '{self.eyes}', '{self.hairs}')"
+    
+    def __repr__(self):
+        """Return official string representation of the Lannister character"""
+        return self.__str__()
+
+    @classmethod
+    def create_lannister(cls, first_name, is_alive=True):
+        """Class method to create a Lannister character."""
+        return cls(first_name, is_alive)

e81d3db774356064ca03373e4f60a678b8b16525
Kaouthar Kouaz <kkouaz@e2r3p2.1337.ma>
2025-12-13 11:19:16 +0100

    finito

---
+++ python-4-dod/ex03/new_student.py
@@ -0,0 +1,20 @@
+import random
+import string
+from dataclasses import dataclass, field
+
+
+def generate_id() -> str:
+    return "".join(random.choices(string.ascii_lowercase, k=15))
+
+
+@dataclass
+class Student:
+    name: str
+    surname: str
+    active: bool = True
+    login: str = field(init=False)
+    id: str = field(init=False, default_factory=generate_id)
+
+    def __post_init__(self):
+        """Create login after initialization."""
+        self.login = f"{self.name[0]}{self.surname}".lower()

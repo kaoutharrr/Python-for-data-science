b96c8f5d599281cd6c9b09680e6b327a24a79442
Kaouthar Kouaz <kkouaz@e2r3p2.1337.ma>
2025-12-13 10:18:39 +0100

    aintnomercy

--- python-4-dod/ex01/in_out.py
+++ python-4-dod/ex01/in_out.py
@@ -2,11 +2,20 @@ def square(x: int | float) -> int | float:
     """sqaure of x"""
     return x*x
 
+
 def pow(x: int | float) -> int | float:
     """power of x"""
     return x**x
 
+
 def outer(x: int | float, function) -> object:
     """Return a closure that applies function repeatedly to x."""
-#     count = 0
-# def inner() -> float
+    count = 0
+    res = x
+
+    def inner() -> float:
+        nonlocal count, res
+        count += 1
+        res = function(res)
+        return res
+    return inner
